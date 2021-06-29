09323747cc2e851f8b6c9691e36dbfd5bdc53f47
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-06-29 18:44:47 +0000

    Task 2

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,8 @@
+
+function welcomeMessage(fullname)
+{
+    return alert("Welcome " + fullname);
+}
+var guillaume = () => welcomeMessage("Guillaume");
+var alex = () => welcomeMessage("Alex");
+var fred = () => welcomeMessage("Fred");
