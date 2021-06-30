8ff89237c4dbea714f5629cd83345b5a20679842
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-06-30 19:21:44 +0000

    Classroom

---
+++ 0x0C-Javascript_advanced/3-classrooms.js
@@ -0,0 +1,13 @@
+
+function createClassRoom(numbersOfStudents){
+    function studentSeat(seat){
+        
+    }
+    }
+    let students = [];
+    for (let i = 0; i < numbersOfStudents; i++) {
+        students.push(studentsSeat(i + 1))
+    }
+    return students;
+}
+const classRoom = 
